cd8568c3f705ac70114cba12c4e6daf2c61b2d5a
ksquaredcoding <ksquaredcoding@gmail.com>
2023-02-06 16:02:45 -0700

    tsc -w to watch all files

--- basics.ts
+++ basics.ts
@@ -1,4 +1,4 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function add1(n1: number, n2: number, showResult: boolean, phrase: string) {
   if (showResult) {
     const result = n1 + n2
     console.log(phrase + result);
@@ -13,5 +13,5 @@ const number2 = 2.8;
 const printResult = true;
 const resultPhrase = 'Result is: ';
 
-add(number1, number2, printResult, resultPhrase)
+add1(number1, number2, printResult, resultPhrase)
 
