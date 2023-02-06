e4d26590b4d5464c06cecd2b59e8df99a56529d7
ksquaredcoding <ksquaredcoding@gmail.com>
2023-02-06 15:05:52 -0700

    literal types, unary plus + parseFloat() alt

--- app.ts
+++ app.ts
@@ -1,15 +1,23 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(input1: number | string, input2: number | string, resultConverison: 'as-number' | 'as-text') {
   let result
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConverison === 'as-number') {
+    result = +input1 + +input2
   } else {
     result = input1.toString() + input2.toString()
   }
-  return result;
+  // if (resultConverison === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+  return result
 }
 
-const combinedAges = combine(26, 22)
+const combinedAges = combine(26, 22, 'as-number')
 console.log(combinedAges);
 
-const combinedNames = combine('Kevin', 'Val');
+const combinedStringAges = combine('26', '22', 'as-number')
+console.log(combinedStringAges);
+
+const combinedNames = combine('Kevin', 'Val', 'as-text');
 console.log(combinedNames);
