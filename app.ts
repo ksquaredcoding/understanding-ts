02a3e645a3ec385dde7844189385daeba61cb4af
ksquaredcoding <ksquaredcoding@gmail.com>
2023-02-06 15:42:26 -0700

    functions done, unknown and never types now

--- app.ts
+++ app.ts
@@ -1,15 +1,9 @@
-function add(n1: number, n2: number) {
-  return n1 + n2
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = 'Kevin'
+if (typeof userInput === 'string') {
+  userName = userInput
 }
 
-function printResult(num: number) {
-  console.log('Result: ' + num);
-}
-
-printResult(add(5, 12))
-
-let combineValues: (a: number, b: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
